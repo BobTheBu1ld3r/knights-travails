99f83e550dd9e1484c8e6f8fefd26fdaab68985e
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-11-23 12:39:40 +0000

    Define isValid() function which checks validity of given square

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ Queue.prototype.dequeue = function () {
 Queue.prototype.isEmpty = function () {
   return this.items.length === 0;
 };
+
+function isValid([x, y]) {
+  const X_MIN = 0;
+  const X_MAX = 7;
+  const Y_MIN = 0;
+  const Y_MAX = 7;
+
+  return x >= X_MIN && x <= X_MAX && y >= Y_MIN && y <= Y_MAX;
+}
