7e0ee4b48c46ead362805a199777f161e17fce69
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-11-23 14:54:42 +0000

    Create buildGraph function for chess board

--- index.js
+++ index.js
@@ -5,17 +5,69 @@ Queue.prototype.enqueue = function (newItem) {
   this.items.push(newItem);
 };
 Queue.prototype.dequeue = function () {
-  this.items.shift();
+  return this.items.shift();
 };
 Queue.prototype.isEmpty = function () {
   return this.items.length === 0;
 };
 
-function isValid([x, y]) {
-  const X_MIN = 0;
-  const X_MAX = 7;
-  const Y_MIN = 0;
-  const Y_MAX = 7;
+const X_MIN = 0;
+const X_MAX = 3;
+const Y_MIN = 0;
+const Y_MAX = 3;
 
+function isValid([x, y]) {
   return x >= X_MIN && x <= X_MAX && y >= Y_MIN && y <= Y_MAX;
 }
+
+function computeIndex([x, y]) {
+  return x + y * (Y_MAX + 1);
+}
+
+function getAvailablePositions([x, y]) {
+  const MOVES = [
+    [2, 1],
+    [2, -1],
+    [1, 2],
+    [1, -2],
+    [-2, 1],
+    [-2, -1],
+    [-1, 2],
+    [-1, -2],
+  ];
+
+  const availablePositions = MOVES.map((move) => {
+    const moveX = move[0];
+    const moveY = move[1];
+    return [x + moveX, y + moveY];
+  }).filter((position) => isValid(position));
+
+  return availablePositions;
+}
+
+function buildGraph(source = [0, 0]) {
+  const adjList = [];
+  const queue = new Queue();
+  const sourceVertex = {
+    index: computeIndex(source),
+    position: source,
+  };
+  queue.enqueue(sourceVertex);
+  while (!queue.isEmpty()) {
+    const currentVertex = queue.dequeue();
+    const availablePositions = getAvailablePositions(currentVertex.position);
+    adjList[currentVertex.index] = availablePositions.map((position) => {
+      const newVertex = {
+        index: computeIndex(position),
+        position: position,
+      };
+      if (!adjList[newVertex.index]) queue.enqueue(newVertex);
+      return newVertex;
+    });
+  }
+
+  return adjList;
+}
+
+const adjList = buildGraph([0, 0]);
+debugger;
